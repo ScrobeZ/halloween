f4178f547f91bc55d9d8ce87eb8368b416d1b441
ScrobeZ <jorgeluis-sc@outlook.es>
2023-11-07 21:42:17 -0800

    Implementacion de cards_screen

--- lib/config/menu/menu_items.dart
+++ lib/config/menu/menu_items.dart
@@ -1,11 +1,10 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class MenuItem {
+  final String screenName;
   final String title;
   final String subTitle;
   final String link;
-  final String screenName;
   final IconData icon;
 
   const MenuItem(
@@ -16,19 +15,17 @@ class MenuItem {
       required this.screenName});
 }
 
-const appMenuItems = <MenuItem>[
-  MenuItem(
-    title: 'Botones',
-    subTitle: 'Varios botones en Flutter',
-    link: '/buttons',
-    icon: Icons.smart_button_outlined,
-    screenName: 'button_screen',
-  ),
-  MenuItem(
-    title: 'Tarjetas',
-    subTitle: 'un contenedor estilizado',
-    link: '/card',
-    icon: Icons.credit_card,
-    screenName: 'card_screen',
-  )
+List<MenuItem> appMenuItems = <MenuItem>[
+  const MenuItem(
+      title: 'Botones',
+      subTitle: 'Varios botones en Flutter',
+      link: '/buttons',
+      icon: Icons.smart_button_outlined,
+      screenName: 'button_screen'),
+  const MenuItem(
+      title: 'Tarjetas',
+      subTitle: 'un contenedor estilizado',
+      link: '/cards',
+      icon: Icons.credit_card,
+      screenName: 'card_screen'),
 ];
